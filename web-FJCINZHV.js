a1309be46d338554d37f03cc58638ca2aa6cc26c
elveahuang <elvea.huang@gmail.com>
2025-12-05 15:53:19 +0000

    Deploying to gh-pages from @ elveahuang/ionic-boilerplate@8dc8a77bbba0117e6c6044bef88795827eaf0a92 🚀

---
+++ web-FJCINZHV.js
@@ -0,0 +1,1 @@
+import{c as n}from"./chunk-VDHL3VED.js";import"./chunk-V6SBLSES.js";var e=class extends n{async show(o){}async hide(o){}};export{e as SplashScreenWeb};
--- web-HHTTIVZL.js
+++
@@ -1,1 +0,0 @@
-import{c as n}from"./chunk-U6IEH4XE.js";import"./chunk-V6SBLSES.js";var e=class extends n{async show(o){}async hide(o){}};export{e as SplashScreenWeb};
