ebef5a838eec8592d4475373afde0ffc8744241e
Elvea Huang <elvea.huang@gmail.com>
2026-08-29 10:37:15 +0800

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { AppComponent } from '@/app/app.component';
 import { routes } from '@/app/app.routes';
 import { CoreModule } from '@/app/core/core.module';
-import { environment } from '@/environments/environment';
-import { ApplicationConfig, enableProdMode, importProvidersFrom, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { PreloadAllModules, provideRouter, RouteReuseStrategy, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular';
 import { register } from 'swiper/element/bundle';
-
-if (environment.production) {
-    enableProdMode();
-}
 
 export const config: ApplicationConfig = {
     providers: [
